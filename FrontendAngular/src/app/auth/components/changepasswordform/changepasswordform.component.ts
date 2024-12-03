83e8ff614c5053f2659393e246310abfd5eeaa9a
alvarosalamancax <alvaro.salamancape@gmail.com>
2024-12-03 20:57:35 -0300

    front de changePassword y sus componentes
    changepassword codigo de sus componentes
    03-12-2024
    ALVARO

--- FrontendAngular/src/app/auth/components/changepasswordform/changepasswordform.component.ts
+++ FrontendAngular/src/app/auth/components/changepasswordform/changepasswordform.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-changepasswordform',
-  standalone: true,
-  imports: [],
   templateUrl: './changepasswordform.component.html',
-  styleUrl: './changepasswordform.component.css'
+  styleUrls: ['./changepasswordform.component.css']
 })
-export class ChangepasswordformComponent {
+export class ChangePasswordFormComponent {
+  passwordForm: FormGroup;
+  errorMessage: string | null = null;
 
+  constructor(private fb: FormBuilder) {
+    this.passwordForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      currentPassword: ['', Validators.required],
+      newPassword: ['', [Validators.required, Validators.pattern(/^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/)]],
+      confirmPassword: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.passwordForm.value.newPassword !== this.passwordForm.value.confirmPassword) {
+      this.errorMessage = 'Las contraseñas no coinciden.';
+    } else {
+      this.errorMessage = null;
+      alert('Contraseña cambiada exitosamente.');
+    }
+  }
 }
