bc672c53e17e5abdfc0e4dbabd287cef7556b059
alvarosalamancax <alvaro.salamancape@gmail.com>
2024-12-03 21:34:44 -0300

    UPDATE LOGIN.COMPONENT.TS
    ESCRIBO EL CODIGO DE LOGIN.COMPONENT.TS
    ALVARO
    03-12-2024

--- FrontendAngular/src/app/auth/components/loginform/loginform.component.ts
+++ FrontendAngular/src/app/auth/components/loginform/loginform.component.ts
@@ -1,12 +1,26 @@
+// login.component.ts
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-loginform',
-  standalone: true,
-  imports: [],
-  templateUrl: './loginform.component.html',
-  styleUrl: './loginform.component.css'
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
 })
-export class LoginformComponent {
+export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log('Formulario Enviado', this.loginForm.value);
+      // Implementa aquí la lógica de autenticación o llamada a la API
+    }
+  }
 }
