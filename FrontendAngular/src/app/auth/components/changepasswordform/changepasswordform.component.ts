401827a4b9dc0c79dee801809dabdfa2c78ba41b
alvarosalamancax <alvaro.salamancape@gmail.com>
2024-12-03 21:01:10 -0300

    tailwind.config.js
    configuro y se agrega el tailwind.config.js para que tailwing abarque diseño de html y css. /** @type {import('tailwindcss').Config} */
    module.exports = {
      content: ['./src/**/*.{html,ts}'],
      theme: {
        extend: {},
      },
      plugins: [],
    }

--- FrontendAngular/src/app/auth/components/changepasswordform/changepasswordform.component.ts
+++ FrontendAngular/src/app/auth/components/changepasswordform/changepasswordform.component.ts
@@ -1,30 +1,12 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-changepasswordform',
+  standalone: true,
+  imports: [],
   templateUrl: './changepasswordform.component.html',
-  styleUrls: ['./changepasswordform.component.css']
+  styleUrl: './changepasswordform.component.css'
 })
-export class ChangePasswordFormComponent {
-  passwordForm: FormGroup;
-  errorMessage: string | null = null;
+export class ChangepasswordformComponent {
 
-  constructor(private fb: FormBuilder) {
-    this.passwordForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      currentPassword: ['', Validators.required],
-      newPassword: ['', [Validators.required, Validators.pattern(/^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/)]],
-      confirmPassword: ['', Validators.required]
-    });
-  }
-
-  onSubmit() {
-    if (this.passwordForm.value.newPassword !== this.passwordForm.value.confirmPassword) {
-      this.errorMessage = 'Las contraseñas no coinciden.';
-    } else {
-      this.errorMessage = null;
-      alert('Contraseña cambiada exitosamente.');
-    }
-  }
 }
