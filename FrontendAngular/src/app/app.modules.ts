2dbb721b1fcf35d17c1f888b634dba252977961e
alvarosalamancax <alvaro.salamancape@gmail.com>
2024-12-04 21:59:07 -0300

    codigo de registerform

--- FrontendAngular/src/app/app.modules.ts
+++ FrontendAngular/src/app/app.modules.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // Necesario para ngModel
+import { RouterModule } from '@angular/router'; // Rutas necesarias
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+import { AppComponent } from './app.component';
+import { RentBookMovieFormComponent } from './client/components/rentbookmovieform/rentbookmovieform.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RentBookMovieFormComponent // Declaración del componente
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule, // Soporte para formularios
+    RouterModule.forRoot([ // Configuración de rutas
+      { path: '', redirectTo: 'rentbook', pathMatch: 'full' },
+      { path: 'rentbook', component: RentBookMovieFormComponent }
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent] // Bootstrap inicial
 })
-export class AppComponent {
-  title = 'tu-proyecto-angular';
-}
+export class AppModule {}
--- FrontendAngular/src/app/auth/components/registerform/registerform.component.ts
+++ FrontendAngular/src/app/auth/components/registerform/registerform.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-registerform',
-  standalone: true,
-  imports: [],
+  selector: 'app-register-form',
   templateUrl: './registerform.component.html',
-  styleUrl: './registerform.component.css'
+  styleUrls: ['./registerform.component.css']
 })
-export class RegisterformComponent {
+export class RegisterFormComponent {
+  showModal = false;
+  showSuccessModal = false;
 
+  openModal() {
+    this.showModal = true;
+  }
+
+  closeModal() {
+    this.showModal = false;
+  }
+
+  confirmRegistration() {
+    this.showModal = false;
+    this.showSuccessModal = true;
+  }
+
+  closeSuccessModal() {
+    this.showSuccessModal = false;
+  }
 }
