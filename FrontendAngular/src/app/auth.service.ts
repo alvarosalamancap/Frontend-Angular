66eaa15d7af7ef2d5759742f606b803b54da6b58
alvarosalamancax <alvaro.salamancape@gmail.com>
2024-12-08 10:27:13 -0300

    Update auth.service.ts
    import { Injectable } from '@angular/core';
    import { HttpClient } from '@angular/common/http';
    import { Observable } from 'rxjs';
    
    @Injectable({
      providedIn: 'root'
    })
    export class AuthService {
      private apiUrl = 'http://tu-backend.test/api'; // Cambia esto según tu URL
    
      constructor(private http: HttpClient) {}
    
      login(email: string, password: string): Observable<any> {
        return this.http.post(`${this.apiUrl}/login`, { email, password });
      }
    }

--- FrontendAngular/src/app/auth.service.ts
+++ FrontendAngular/src/app/auth.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private apiUrl = 'http://tu-backend.test/api'; // Cambia esto según tu URL
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  login(email: string, password: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, { email, password });
+  }
 }
