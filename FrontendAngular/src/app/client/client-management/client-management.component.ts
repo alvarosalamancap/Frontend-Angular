e5e61a2a1b34a02ff1c9d4895e838cd848b1a36a
alvarosalamancax <alvaro.salamancape@gmail.com>
2024-12-09 13:00:08 -0300

    front de gestionar clientes
    subo todos los componentes y codigo de gestionar clientes.
    client-management
    ALVARO
    09-12-2024

---
+++ FrontendAngular/src/app/client/client-management/client-management.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-management',
+  standalone: true,
+  imports: [],
+  templateUrl: './client-management.component.html',
+  styleUrl: './client-management.component.css'
+})
+export class ClientManagementComponent {
+
+}
